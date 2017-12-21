6410be17574398ed023359746c903aae677028cb
Ravindra Sakpal <sunlax83@gmail.com>
2017-12-22 10:37:07 +1100

    Add project page

--- server.js
+++ server.js
@@ -21,7 +21,6 @@ app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
 
-
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -42,6 +41,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
